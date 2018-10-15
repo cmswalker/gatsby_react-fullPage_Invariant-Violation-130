68b0c4575a2c116daa25f38f802ebee7e9fe9234
Michael Walker <cmichaelswalker@gmail.com>
2018-10-14 17:09:15 -0700

    Added commonjs bundle to work with gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,48 +1,51 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from "react";
-import ReactFullpage from "@fullpage/react-fullpage";
+import ReactFullpage from "@fullpage/react-fullpage/dist/react-fullpage-commonjs";
 
+class FullpageWrapper extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
+  onLeave() {
+    console.log('on leave', arguments);
+  }
 
-const FullpageWrapper = fullpageProps => (
-  <ReactFullpage
-    {...fullpageProps}
-    render={({ state, fullpageApi }) => {
-      console.log("render prop change", state); // eslint-disable-line no-console
+  render() {
+    return (
+      <ReactFullpage
+        onLeave={this.onLeave.bind(this)}
+        render={({ state, fullpageApi }) => {
+          console.log("render prop change", state, fullpageApi); // eslint-disable-line no-console
 
-      if (state.callback === "onLeave") {
-        if (state.direction === "down") {
-          console.log("going down..." + state.origin.index);
-        }
-      }
-      return (
-        <div id="fullpage-wrapper">
-          <div className="section section1">
-            <h3>Section 1</h3>
-            <button onClick={() => fullpageApi.moveSectionDown()}>
-              Move down
-            </button>
-          </div>
-          <div className="section">
-            <div className="slide">
-              <h3>Slide 2.1</h3>
+          return (
+            <div id="fullpage-wrapper">
+              <div className="section section1">
+                <h3>Section 1</h3>
+                <button onClick={() => fullpageApi.moveSectionDown()}>
+                  Move down
+                </button>
+              </div>
+              <div className="section">
+                <div className="slide">
+                  <h3>Slide 2.1</h3>
+                </div>
+                <div className="slide">
+                  <h3>Slide 2.2</h3>
+                </div>
+                <div className="slide">
+                  <h3>Slide 2.3</h3>
+                </div>
+              </div>
+              <div className="section">
+                <h3>Section 3</h3>
+              </div>
             </div>
-            <div className="slide">
-              <h3>Slide 2.2</h3>
-            </div>
-            <div className="slide">
-              <h3>Slide 2.3</h3>
-            </div>
-          </div>
-          <div className="section">
-            <h3>Section 3</h3>
-          </div>
-        </div>
-      );
-    }}
-  />
-);
-
-
+          );
+        }}
+      />
+    );
+  }
+};
 
 export default FullpageWrapper;
